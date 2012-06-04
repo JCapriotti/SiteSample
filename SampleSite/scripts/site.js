1624fceeaee393094848261d3396d5f4d0612896
Jason Capriotti <jason.capriotti@gmail.com>
2012-06-04 16:00:54 -0500

    Updated transitions for Finch

--- SampleSite/scripts/site.js
+++ SampleSite/scripts/site.js
@@ -1,7 +1,25 @@
 ﻿///<reference path="finch.js"/>
 $(function () {
-	Finch.route("about", function () {
+	//	Finch.route("about", function () {		
+	//	});
+
+	//	Finch.route("contact", function () {
+	//	});
+
+	Finch.route("", function () {
+		loadContent("index");
+	});
+
+	Finch.route("/:page", function (bindings) {
+		loadContent(bindings.page);
 	});
 
 	Finch.listen();
 });
+
+
+function loadContent(view) {
+	$.get("./views/" + view + ".tmpl", function (data) {
+		$("#mainContent").html(data);
+	});
+}
